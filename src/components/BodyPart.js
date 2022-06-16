9dfc98dece9f1e18a9486b51eea20b2c4fc9241e
momen <momen885@yahoo.com>
2022-06-16 18:08:38 +0200

    bodyPart styles

--- src/components/BodyPart.js
+++ src/components/BodyPart.js
@@ -1,7 +1,37 @@
 import React from "react";
+import { Stack, Typography } from "@mui/material";
+import Icon from "../assets/icons/gym.png";
 
-const BodyPart = () => {
-  return <div>BodyPart</div>;
+const BodyPart = ({ item, setBodyPart, bodyPart }) => {
+  return (
+    <Stack
+      type="button"
+      alignItems="center"
+      justifyContent="center"
+      className="bodyPart-card"
+      sx={
+        bodyPart === item
+          ? {
+              borderTop: "4px solid #ff2625",
+              backgroundColor: "#fff",
+              borderBottomLeftRadius: "20px",
+              width: "270px",
+              height: "280px",
+              cursor: "pointer",
+              gap: "47px",
+            }
+          : {
+              background: "#fff",
+            }
+      }
+    >
+      <img
+        src={Icon}
+        alt="dumbbell"
+        style={{ width: "40px", height: "40px" }}
+      />
+    </Stack>
+  );
 };
 
 export default BodyPart;
